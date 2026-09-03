725a32db65f5eca009a24f43ba82267ac2f20e7b
Am0rr <itsaizel468@gmail.com>
2026-09-03 13:43:45 +0300

    feat(bll): add IEndpointService interface

---
+++ backend/AM.BLL/Interfaces/IEndpointService.cs
@@ -0,0 +1,13 @@
+using AM.BLL.DTOs.Endpoints;
+
+namespace AM.BLL.Interfaces;
+
+public interface IEndpointService
+{
+    Task<EndpointResponse> CreateAsync(CreateEndpointRequest request, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Guid endpointId, UpdateEndpointRequest request, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid endpointId, CancellationToken cancellationToken = default);
+    Task<EndpointResponse?> GetByIdAsync(Guid endpointId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<EndpointResponse>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<EndpointResponse>> GetActiveAsync(CancellationToken cancellationToken = default);
+}
