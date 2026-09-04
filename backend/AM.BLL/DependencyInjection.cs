ef4fe79675156a321e33dddd88be8c0d572ee033
Am0rr <itsaizel468@gmail.com>
2026-09-04 12:26:37 +0300

    feat(bll): add dependency injection registration

---
+++ backend/AM.BLL/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using AM.BLL.Interfaces;
+using AM.BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AM.BLL;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessLogicLayer(IServiceCollection services)
+    {
+        services.AddScoped<IEndpointService, EndpointService>();
+        services.AddScoped<IExecutionLogService, ExecutionLogService>();
+
+        services.AddHostedService<MonitoringBackgroundService>();
+
+        return services;
+    }
+}
